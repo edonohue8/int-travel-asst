06434ba7ab991d4356176d1b7bd33a04a62a24cc
Joe <joseph@teloconsolutions.com>
2020-09-15 16:40:57 -0700

    last updates

--- assets/flightInfo.js
+++ assets/flightInfo.js
@@ -1,90 +1,90 @@
 {2 items
-"type":"object"
-    "properties":{5 items
-        "Dates":{2 items
-            "type":"object"
-                "properties":{2 items
-                    "OutboundDates":{2 items
-                        "type":"array"
-                            "items":{1 item
-                                "type":"object"
+    "type":"object"
+        "properties":{5 items
+            "Dates":{2 items
+                "type":"object"
+                    "properties":{2 items
+                        "OutboundDates":{2 items
+                            "type":"array"
+                                "items":{1 item
+                                    "type":"object"
+                                }
+                        }
+                        "InboundDates":{2 items
+                            "type":"array"
+                                "items":{1 item
+                                    "type":"object"
+                                }
+                        }
+                    }
+            }
+            "Quotes":{2 items
+                "type":"array"
+                    "items":{1 item
+                        "type":"object"
+                    }
+            }
+            "Places":{2 items
+                "type":"array"
+                    "items":{2 items
+                        "type":"object"
+                            "properties":{8 items
+                                "PlaceId":{...}1 item
+                                "IataCode":{...}1 item
+                                "Name":{...}1 item
+                                "Type":{...}1 item
+                                "SkyscannerCode":{...}1 item
+                                "CityName":{...}1 item
+                                "CityId":{...}1 item
+                                "CountryName":{...}1 item
                             }
                     }
-                    "InboundDates":{2 items
-                        "type":"array"
-                            "items":{1 item
-                                "type":"object"
+            }
+            "Carriers":{2 items
+                "type":"array"
+                    "items":{2 items
+                        "type":"object"
+                            "properties":{2 items
+                                "CarrierId":{1 item
+                                    "type":"integer"
+                                }
+                                "Name":{1 item
+                                    "type":"string"
+                                }
                             }
                     }
-                }
+            }
+            "Currencies":{2 items
+                "type":"array"
+                    "items":{2 items
+                        "type":"object"
+                            "properties":{8 items
+                                "Code":{1 item
+                                    "type":"string"
+                                }
+                                "Symbol":{1 item
+                                    "type":"string"
+                                }
+                                "ThousandsSeparator":{1 item
+                                    "type":"string"
+                                }
+                                "DecimalSeparator":{1 item
+                                    "type":"string"
+                                }
+                                "SymbolOnLeft":{1 item
+                                    "type":"boolean"
+                                }
+                                "SpaceBetweenAmountAndSymbol":{1 item
+                                    "type":"boolean"
+                                }
+                                "RoundingCoefficient":{1 item
+                                    "type":"integer"
+                                }
+                                "DecimalDigits":{1 item
+                                    "type":"integer"
+                                }
+                            }
+                    }
+            }
         }
-        "Quotes":{2 items
-            "type":"array"
-                "items":{1 item
-                    "type":"object"
-                }
-        }
-        "Places":{2 items
-            "type":"array"
-                "items":{2 items
-                    "type":"object"
-                        "properties":{8 items
-                            "PlaceId":{...}1 item
-                            "IataCode":{...}1 item
-                            "Name":{...}1 item
-                            "Type":{...}1 item
-                            "SkyscannerCode":{...}1 item
-                            "CityName":{...}1 item
-                            "CityId":{...}1 item
-                            "CountryName":{...}1 item
-                        }
-                }
-        }
-        "Carriers":{2 items
-            "type":"array"
-                "items":{2 items
-                    "type":"object"
-                        "properties":{2 items
-                            "CarrierId":{1 item
-                                "type":"integer"
-                            }
-                            "Name":{1 item
-                                "type":"string"
-                            }
-                        }
-                }
-        }
-        "Currencies":{2 items
-            "type":"array"
-                "items":{2 items
-                    "type":"object"
-                        "properties":{8 items
-                            "Code":{1 item
-                                "type":"string"
-                            }
-                            "Symbol":{1 item
-                                "type":"string"
-                            }
-                            "ThousandsSeparator":{1 item
-                                "type":"string"
-                            }
-                            "DecimalSeparator":{1 item
-                                "type":"string"
-                            }
-                            "SymbolOnLeft":{1 item
-                                "type":"boolean"
-                            }
-                            "SpaceBetweenAmountAndSymbol":{1 item
-                                "type":"boolean"
-                            }
-                            "RoundingCoefficient":{1 item
-                                "type":"integer"
-                            }
-                            "DecimalDigits":{1 item
-                                "type":"integer"
-                            }
-                        }
-                }
-        }
-    }
 }
