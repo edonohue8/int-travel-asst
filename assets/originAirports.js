efcd7edc7107a40ff8e33c66e42abcef641201b7
Ekow <edonohue8@users.noreply.github.com>
2020-09-17 19:21:28 -0700

    comments added

--- assets/originAirports.js
+++ assets/originAirports.js
@@ -1,81 +1,4 @@
 //list object of all medium and large hub international airports in the USA
-<<<<<<< HEAD
-
-var originAirports = {
-    'Albuquerque, NM': 'ABQ',
-    'Anchorage, AK': 'ANC',
-    'Atlanta, GA': 'ATL',
-    'Austin, TX': 'AUS',
-    'Baltimore, MD': 'BWI',
-    'Boston, MA': 'BOS',
-    'Buffalo, NY': 'BUF',
-    'Burbank, CA': 'BUR',
-    'Charlotte, NC': 'CLT',
-    'Chicago, IL - Midway': 'MDW',
-    'Chicago, IL - OHare': 'ORD',
-    'Cincinnati, OH': 'CVG',
-    'Cleveland, OH': 'CLE',
-    'Columbus, OH': 'CMH',
-    'Dallas/Fort Worth, TX': 'DFW',
-    'Denver, CO - International': 'DEN',
-    'Devils Lake, ND': 'DVL',
-    'Durham/Raleigh, NC': 'RDU',
-    'El Paso, TX': 'ELP',
-    'Fort Lauderdale, FL': 'FLL',
-    'Fort Myers, FL': 'RSW',
-    'Fort Worth/Dallas, TX': 'DFW',
-    'Hartford, CT': 'BDL',
-    'Honolulu, HI - Island of Oahu': 'HNL',
-    'Houston, TX - Hobby': 'HOU',
-    'Houston, TX - Intercontinental': 'IAH',
-    'Indianapolis, IN': 'IND',
-    'Jacksonville, FL': 'JAX',
-    'Jamestown, ND': 'JMS',
-    'Kahului, HI - Island of Maui': 'OGG',
-    'Kansas City, MO': 'MCI',
-    'Las Vegas, NV': 'LAS',
-    'Los Angeles, CA': 'LAX',
-    'Miami, FL - International': 'MIA',
-    'Milwaukee, WI': 'MKE',
-    'Minneapolis, MN': 'MSP',
-    'Nashville, TN': 'BNA',
-    'New Orleans, LA': 'MSY',
-    'New York, NY - Kennedy': 'JFK',
-    'New York, NY - La Guardia': 'LGA',
-    'Newark, NJ': 'EWR',
-    'Oakland, CA': 'OAK',
-    'Omaha, NE': 'OMA',
-    'Ontario, CA': 'ONT',
-    'Orange County, CA': 'SNA',
-    'Orlando, FL - International': 'MCO',
-    'Philadelphia, PA - International': 'PHL',
-    'Phoenix, AZ': 'PHX',
-    'Pittsburgh, PA': 'PIT',
-    'Portland, OR': 'PDX',
-    'Raleigh/Durham, NC': 'RDU',
-    'Sacramento, CA': 'SMF',
-    'Saint Louis, MO': 'STL',
-    'Salt Lake City, UT': 'SLC',
-    'San Antonio, TX': 'SAT',
-    'San Diego, CA': 'SAN',
-    'San Francisco, CA': 'SFO',
-    'San Jose, CA': 'SJC',
-    'San Juan, Puerto Rico': 'SJU',
-    'Seattle, WA - Seattle/Tacoma International': 'SEA',
-    'Tampa, FL': 'TPA',
-    'Washington DC - Dulles': 'IAD',
-    'Washington DC - National': 'DCA',
-    'West Palm Beach, FL': 'PBI',
-}
-
-var airportList = Object.keys(originAirports);
-
-//  Receives input after each onkeydown or onkeyup
-//  Put that input through the airportList array and if the input matches starting at index 0
-//  For each match, using something like indexOf, then you add the entire string to a separate array
-//  Once you've iterated through the airportList, use that array to construct your dropdown
-//  Clear the previous dropdown and then create the new dropdown
-=======
     const originAiport = {
     'Albuquerque, NM' : 'ABQ',
     'Anchorage, AK' : 'ANC',
@@ -138,4 +61,9 @@ var airportList = Object.keys(originAirports);
     'Washington DC' : 'DCA',
     'West Palm Beach, FL' : 'PBI',
     }
->>>>>>> 69e1d34746f94b8314cad4e430ee14241306a21b
+
+//  Receives input after each onkeydown or onkeyup
+//  Put that input through the airportList array and if the input matches starting at index 0
+//  For each match, using something like indexOf, then you add the entire string to a separate array
+//  Once you've iterated through the airportList, use that array to construct your dropdown
+//  Clear the previous dropdown and then create the new dropdown
